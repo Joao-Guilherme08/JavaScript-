92e246b2c6a514f926eed000f8358170c2436e66
João Guilherme Dias <Alwaysstrong08@hotmail.com>
2025-02-10 22:34:33 -0300

    Create script.js

---
+++ Projeto/script.js
@@ -0,0 +1,19 @@
+
+//var
+//const
+//console.log(100)
+//console.log(200)
+//console.log(idade)
+//console.log(10 + 5)
+//console.log(10 + idade)
+//console.log(10 + nome) //concatenacao 
+let idade = 90
+let nome = "João"
+let logado = true 
+
+
+
+
+
+
+
